16d79c1b1895cc28dd01501c4c805a63cbe1cdf7
David Asabina <vid@bina.me>
2023-03-25 06:30:08 +0000

    chore: Reduce problematic code

--- problem.py
+++ problem.py
@@ -1,23 +1,7 @@
 from pydantic import BaseModel, Field
-from pydantic.dataclasses import dataclass as pydataclass
 
-class ItemData(BaseModel):
+class Thing(BaseModel):
     name: str = Field(example="Something fun")
-
-
-class ItemId(BaseModel):
-    id: str = Field(description="identifier of item", example="xyz123")
-
-
-class Item(ItemId, ItemData):
-    pass
-
-
-@pydataclass
-class ItemList(BaseModel):
-    has_more: bool
-    members: list[Item]
-
 
 if __name__ == "__main__":
     print("here is trouble")
